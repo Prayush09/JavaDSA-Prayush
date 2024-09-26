05883ee4ad134d1de6b232278d23d24f6ee2068f
Prayush Giri <prayushgiri@gmail.com>
2024-09-26 16:45:39 +0530

    Data Structures and some problems

--- Algorithms/GraphTraversals/Depth_First_Search.java
+++ Algorithms/GraphTraversals/Depth_First_Search.java
@@ -1,4 +1,16 @@
 package com.Data_Structures_byprayush.Algorithms.GraphTraversals;
 
+import java.util.List;
+
+
 public class Depth_First_Search {
+    public void dfs(List<List<Integer>> adj, boolean[] visited, int node){
+        //most basic implementation of dfs
+        visited[node] = true;
+        for(int neighbour : adj.get(node)){
+            if(!visited[neighbour]){
+                dfs(adj, visited, neighbour);
+            }
+        }
+    }
 }
